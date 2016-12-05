b23b84d484d2bc2b2085d086ee408c400b0f2606
Paola Katherine Cardoso Pacheco <paolakatherine@MacBook-Air-de-Paola.local>
2016-12-05 13:49:17 -0300

    main

---
+++ lists.py
@@ -0,0 +1,25 @@
+#coding=UTF-8
+def add_numbers(x,y,z):
+    a=[]
+    inicial_num = x
+    size = y
+    qtd_lists = z
+
+    while (qtd_lists > 0):
+        final_num = inicial_num+y
+        a.append(range(inicial_num, final_num))
+        qtd_lists = qtd_lists-1
+        inicial_num = final_num
+    print(a)
+
+
+if __name__ == "__main__":
+    x = input("Qual o número inicial: ")
+    y = input("Qual o tamanho das listas: ")
+    z = input("Qual a quantidade de listas: ")
+
+    add_numbers(x, y, z)
+
+#x - numero inicial (1 = [1,2,3])
+#y - tamanho das listas (2 = [1,2])
+#z - quantidade de listas (3 = [1,2],[3,4],[5,6])
