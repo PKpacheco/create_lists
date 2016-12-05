f7a193c6b5fd6e87ddb7c335519b1df1fc70855b
Paola Katherine Cardoso Pacheco <paolakatherine@MacBook-Air-de-Paola.local>
2016-12-05 14:03:09 -0300

    pep8

--- lists.py
+++ lists.py
@@ -1,14 +1,16 @@
-#coding=UTF-8
-def add_numbers(x,y,z):
-    a=[]
+# coding=UTF-8
+
+
+def add_numbers(x, y, z):
+    a = []
     inicial_num = x
     size = y
     qtd_lists = z
 
     while (qtd_lists > 0):
-        final_num = inicial_num+y
+        final_num = inicial_num + size
         a.append(range(inicial_num, final_num))
-        qtd_lists = qtd_lists-1
+        qtd_lists = qtd_lists - 1
         inicial_num = final_num
     print(a)
 
@@ -20,6 +22,8 @@ if __name__ == "__main__":
 
     add_numbers(x, y, z)
 
-#x - numero inicial (1 = [1,2,3])
-#y - tamanho das listas (2 = [1,2])
-#z - quantidade de listas (3 = [1,2],[3,4],[5,6])
+'''
+x - numero inicial (1 = [1,2,3])
+y - tamanho das listas (2 = [1,2])
+z - quantidade de listas (3 = [1,2],[3,4],[5,6])
+'''
